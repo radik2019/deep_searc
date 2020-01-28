db2a521767c975a63b3e6c3e1922b312e44d4cab
jumperlidia <jumperlidia@hihihi>
2020-01-28 16:37:54 +0100

    un altra funzione

--- proba.py
+++ proba.py
@@ -1,17 +1,13 @@
 import os
-
-file_python = os.getcwd() + "/test_raccolta"
+path = os.getcwd()
+os.chdir(r"C:\\users")
+file_python = path + "/test_raccolta"
 if os.path.exists(file_python):
     pass
 else:
     os.mkdir(file_python)
 
-def cop(i, file_python):
-	with open(i, "rb") as df:
-		s = df.read()
 
-	with open(f"{file_python}/{i}", "wb") as gf:
-		gf.write(s)
 
 def ser(dir, index="py"):
 	for i in os.listdir(dir):
@@ -19,9 +15,27 @@ def ser(dir, index="py"):
 			new = i.split(".")
 			if len(new) == 2:
 				if new[1] == index:
-					cop(i, file_python)
+					with open(i, "rb") as df:
+						s = df.read()
+
+					with open(f"{file_python}/{i}", "wb") as gf:
+						gf.write(s)
 		else:
 			ser(dir+"/"+i)
 
+for dir_, path, file in os.walk(os.getcwd()):
+	for i in file:
+		df = i.split(".")
+		if df[-1] == 'png':
+			print(i)
+			
 
-print(ser(os.getcwd()))
+
+
+
+
+
+
+
+
+# print(ser(os.getcwd()))
